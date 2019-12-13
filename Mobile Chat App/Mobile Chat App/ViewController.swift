2c7890cb3084eb94d443dc62812c8f3e49be7f47
Nasir Ahmed Momin <momin96@gmail.com>
2019-12-13 19:04:30 +0530

    xmppController intlization

--- Mobile Chat App/Mobile Chat App/ViewController.swift
+++ Mobile Chat App/Mobile Chat App/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var xmppController: XMPPController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        try! self.xmppController = XMPPController(withHostName: "host.com", userJID: "user@host.com", hostPort: 5222, password: "password")
+        
+        self.xmppController?.connect()
     }
 
 
