6ca0f4fb12b03f6fa02992bba7746d745d079bc1
Nasir Ahmed Momin <momin96@gmail.com>
2019-12-13 17:50:09 +0530

    Project setup

---
+++ Mobile Chat App/Mobile Chat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile Chat App
+//
+//  Created by Nasir Ahmed Momin on 13/12/19.
+//  Copyright © 2019 Nasir Ahmed Momin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
